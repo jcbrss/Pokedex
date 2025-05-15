3f7e81f611d77593db3902db1de0ec7cb03d3215
jcbrss <julianacarolineh@hotmail.com>
2025-05-15 11:41:29 -0300

    att js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const fetchpokemon = async (pokemon) => {
+    const apiresponse = fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const data = await APIresponse.json();)
+    return data;
+}
+
+
